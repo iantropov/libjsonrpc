9ed71e239bcfb8bb541f654ae56532f2da90170f
Ivan Antropov <antropov.ivan@gmail.com>
2011-02-27 13:03:17 +0600

    JSON + tests + separate suites

---
+++ tests/check_lib.c
@@ -0,0 +1,15 @@
+#include <check.h>
+
+#include "json/check_json.h"
+
+#include <stdlib.h>
+
+int main (void)
+{
+  int number_failed;
+  SRunner *sr = srunner_create(make_json_suite());
+  srunner_run_all (sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed (sr);
+  srunner_free (sr);
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
---
+++ tests/json/check_json.h
@@ -0,0 +1,9 @@
+#ifndef CHECK_JSON_H
+#define CHECK_JSON_H
+
+#include <check.h>
+#include <stdlib.h>
+
+Suite *make_json_suite (void);
+
+#endif
